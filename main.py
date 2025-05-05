15a07832fed62c1bc5f519457293a48c4f95b707
leticiamsdev <leticiamariana2004@gmail.com>
2025-05-05 11:24:41 -0300

    commmit2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
+from datetime import datetime
 
 USERNAME = "leticiamari"
-TOKEN = "wbhdujihawudhoaoaoaoao"
+TOKEN = "wbhdujihawudhoaoaoaoao" #update for environment variable
 GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -30,6 +31,7 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
+today = datetime.now()
 
 #
 # response = requests.post(url=graph_endpoint, json = graph_config, headers=headers)
@@ -38,10 +40,25 @@ headers = {
 att_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 graph_att = {
-    "date" : "20250505",
+    "date" : today.strftime("%Y%m%d"),
     "quantity": "5",
 }
 
+#
+# response = requests.post(url=att_endpoint, json=graph_att, headers=headers)
+# print(response.text)
 
-response = requests.post(url=att_endpoint, json=graph_att, headers=headers)
+
+
+graph_put = {
+    "quantity": "10",
+}
+#
+# put_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+# response = requests.put(url=put_endpoint, json = graph_put, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20250503"
+
+response = requests.delete(url= delete_endpoint, headers=headers)
 print(response.text)
